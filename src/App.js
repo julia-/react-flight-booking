319dc6f545a101474b7200a2510fe61242958565
julia- <julia.ryanx@gmail.com>
2017-11-27 11:21:23 +1100

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Button from './components/Button';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>This is a heading</h1>
+        <Button title='Find Flights' primary={ true } />
+        <Button title='Sign up' />
+        <Button title='Sign in' magic={ true } />
+        <ul>
+          <li>Why</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default App;
